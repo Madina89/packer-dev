dff9fda7901d815e12afb43de73556e4252d9813
Madina89 <madinalinux89@gmail.com>
2019-08-25 17:02:50 -0500

    updated

--- jenkinsfileUpdated.groovy
+++ jenkinsfileUpdated.groovy
@@ -62,11 +62,11 @@ pipeline{
         stage("Filter AMI"){
             steps{
                 script{
-                def AMI
-                if (REGION == "us-east-1"){
-                    AMI = "ami-0b898040803850657"
-                } else if (REGION == "us-east-2"){
-                    AMI =  "ami-0d8f6eb4f641ef961"
+               // def AMI
+                //if (REGION == "us-east-1"){
+                 //  AMI = "ami-0b898040803850657"
+                //} else if (REGION == "us-east-2"){
+                  //  AMI =  "ami-0d8f6eb4f641ef961"
                 }
                 }
             }
